e07c8fa2bf0e06cd88500c80b1f5f418618decbb
matthewchoiyiuhei <matthew.choi@dotdotfire.com>
2021-12-30 12:13:20 +0800

    Updated few function in ArmorStandUtils

--- src/main/java/com/sucy/skill/util/ArmorStandUtils.java
+++ src/main/java/com/sucy/skill/util/ArmorStandUtils.java
@@ -1,23 +1,75 @@
 package com.sucy.skill.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.EulerAngle;
 import org.bukkit.util.Vector;
 
 public class ArmorStandUtils {
+    public static Location faceLocation(Entity entity, Location to) {
+        if (entity.getWorld() != to.getWorld()) {
+            return null;
+        }
+        Location fromLocation = entity.getLocation();
+
+        double xDiff = to.getX() - fromLocation.getX();
+        double yDiff = to.getY() - fromLocation.getY();
+        double zDiff = to.getZ() - fromLocation.getZ();
+
+        double distanceXZ = Math.sqrt(xDiff * xDiff + zDiff * zDiff);
+        double distanceY = Math.sqrt(distanceXZ * distanceXZ + yDiff * yDiff);
+
+        double yaw = Math.toDegrees(Math.acos(xDiff / distanceXZ));
+        double pitch = Math.toDegrees(Math.acos(yDiff / distanceY)) - 90.0D;
+        if (zDiff < 0.0D) {
+            yaw += Math.abs(180.0D - yaw) * 2.0D;
+        }
+        Location loc = entity.getLocation();
+        loc.setYaw((float) (yaw - 90.0F));
+        loc.setPitch((float) (pitch - 90.0F));
+        return loc;
+    }
+    public static EulerAngle convertVectorToEulerAngle(Vector vec) {
+
+        double x = vec.getX();
+        double y = vec.getY();
+        double z = vec.getZ();
+
+        double xz = Math.sqrt(x*x + z*z);
+
+        double eulX;
+        if(x < 0) {
+            if(y == 0) {
+                eulX = Math.PI*0.5;
+            } else {
+                eulX = Math.atan(xz/y)+Math.PI;
+            }
+        } else {
+            eulX = Math.atan(y/xz)+Math.PI*0.5;
+        }
+
+        double eulY;
+        if(x == 0) {
+            if(z > 0) {
+                eulY = Math.PI;
+            } else {
+                eulY = 0;
+            }
+        } else {
+            eulY = Math.atan(z/x)+Math.PI*0.5;
+        }
+
+        return new EulerAngle(eulX, eulY, 0);
+
+    }
     public static void setRotationAccordingToVector(ArmorStand armorStand, Vector vector){
-        Vector normalized = vector.clone().normalize();
-        double deltaX = normalized.getX();
-        double deltaY = normalized.getY();
-        double deltaZ = normalized.getZ();
-        double distance = Math.sqrt(deltaZ * deltaZ + deltaX * deltaX);
-        double pitch = Math.asin(deltaY/distance);
-        double yaw = Math.atan2(deltaX, deltaZ);
-        Bukkit.broadcastMessage("deltaX: "+deltaX);
-        Bukkit.broadcastMessage("deltaY: "+deltaY);
-        Bukkit.broadcastMessage("deltaZ: "+deltaZ);
-        Bukkit.broadcastMessage("pitch: "+pitch);
-        Bukkit.broadcastMessage("yaw: "+yaw);
-        armorStand.setRotation((float)pitch, (float)yaw);
+        EulerAngle eulerAngle = convertVectorToEulerAngle(vector);
+        armorStand.setHeadPose(eulerAngle);
+    }
+    public static void setRotationFacingTarget(ArmorStand armorStand, Entity target){
+        Location newLocation = faceLocation(armorStand, target.getLocation());
+        //armorStand.setHeadPose(newLocation.getDirection());
     }
 }
